3bec7ba82f46403d1b51ab7c23ff3e7758ca22d8
Subin_Moon <smoon44@my.bcit.ca>
2021-03-13 11:02:30 -0800

    Modify game.py

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 """
 Member List
-- Sally
-- Marti
-- Chansaa
-- Subin
+- Batchansaa Batzorig
+- Dmitri Golota
+- Sally Poon
+- Subin Moon
+- Martin Gatchev
 
 Mar 13, 2021
 """
@@ -14,29 +15,50 @@ import random
 from pygame_functions import *
 
 
-
 pygame.init()
 
 WINDOW_SIZE = (500, 500)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 clock = pygame.time.Clock()
 
+
+# Game Functions
+def movement():
+
+    nextFrame = clock()
+    frame = 0
+
+    while True:
+        if clock() > nextFrame:
+            frame = (frame + 1) % 12
+            nextFrame += 120
+        if keyPressed("right"):
+            changeSpriteImage(testSprite, 0*12 + frame)
+        elif keyPressed("down"):
+            changeSpriteImage(testSprite, 1*12 + frame)
+        elif keyPressed("left"):
+            changeSpriteImage(testSprite, 2*12 + frame)
+        elif keyPressed("right"):
+            changeSpriteImage(testSprite, 3*12 + frame)
+
+
+# Game Variables
 bg = pygame.image.load('/assets/background.jpg')
 testSprite = newSprite("./assets/Namu1.gif", 12) # 12 is the gif frame
 
 
-running = True
+game_active = True
 
-while running:
-
+while True:
     for event in pygame.event.get():      # catch all the events that are happening right now
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # image of player
-    # background image
+    if game_active:
+        # Game Functions
+        # image of player
+        # background image
 
     pygame.display.update()
     clock.tick(120)
-
