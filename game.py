bdc713bdd708833607d4c6559f92903e15ade79c
Subin_Moon <smoon44@my.bcit.ca>
2021-03-13 11:59:40 -0800

    Modify score_display() and main loop

--- game.py
+++ game.py
@@ -47,7 +47,6 @@ moving_sprite.add(player)
 
 
 # Game Variables
-
 game_font = pygame.font.Font('./assets/Fipps_font.otf', 7)
 score = 0
 high_score = 100
@@ -55,8 +54,6 @@ high_score = 100
 # Floor_Sand
 sand = pygame.image.load('./assets/sand.png').convert()
 sand = pygame.transform.scale(sand, (512, 512))
-
-
 
 
 # Game Functions
@@ -111,10 +108,19 @@ def movement():
             changeSpriteImage(testSprite, 3*12 + frame)
 
 
-def score_display():
-    score_surface = game_font.render(str(score), True, (255, 255, 255))
-    score_rect = score_surface.get_rect(center=(250, 25))
-    screen.blit(score_surface, score_rect)
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(score), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(250, 25))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(250, 25))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'High score: {int(high_score)}', True, (255, 255, 255))
+        high_score_rect = high_score_surface.get_rect(center=(250, 400))
+        screen.blit(high_score_surface, high_score_rect)
 
 
 # Main Loop
@@ -128,16 +134,19 @@ while game_running:
             pygame.quit()
             sys.exit()
 
+    # Background image
     screen.blit(sand, (0, 0))
 
     if game_active:
-
-        # Game Functions
         # image of player
-        # background image
-        score_display()
         moving_sprite.draw(screen)
 
+        # Game Functions
+        score_display('main_game')
+
+    # game over
+    else:
+        score_display('game_over')
 
     pygame.display.update()
     clock.tick(120)
