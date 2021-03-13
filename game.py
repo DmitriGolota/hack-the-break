3670a6dd3a8c651416a5b4b4fe6a52e8b13e9d6a
Dmitri Golota <dmitri.golota@gmail.com>
2021-03-13 10:53:33 -0800

    Added to movement function

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ import random
 from pygame_functions import *
 
 
+
 pygame.init()
 
 WINDOW_SIZE = (500, 500)
@@ -21,6 +22,8 @@ screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 clock = pygame.time.Clock()
 
 bg = pygame.image.load('/assets/background.jpg')
+testSprite = newSprite(./assets/Namu.gif, 12) # 12 is the gif frame
+
 
 running = True
 
@@ -37,3 +40,14 @@ while running:
     pygame.display.update()
     clock.tick(120)
 
+def movement():
+
+    nextFrame = clock()
+    frame = 0
+
+    while True:
+        if clock() > nextFrame:
+            frame = (frame + 1) % 12
+            nextFrame += 120
+        if keyPressed("right"):
+            changeSpriteImage(testSprite, 0*8)
