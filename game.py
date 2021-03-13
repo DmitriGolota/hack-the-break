09cdadc0b44029aa18720daf0187630fa4695fc2
Sally Poon <sallypoon@hotmail.com>
2021-03-13 10:47:01 -0800

    Try push

--- game.py
+++ game.py
@@ -7,4 +7,29 @@ Member List
 
 Mar 13, 2021
 """
+import pygame
+
+
+pygame.init()
+
+WINDOW_SIZE = (500, 500)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+clock = pygame.time.Clock()
+
+bg = pygame.image.load('/assets/background.jpg')
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():      # catch all the events that are happening right now
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # image of player
+    # background image
+
+    pygame.display.update()
+    clock.tick(120)
 
