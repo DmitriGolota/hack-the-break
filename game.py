b103b01b6d4de67ead3c7af37c9f2bec7404741f
Subin_Moon <smoon44@my.bcit.ca>
2021-03-13 10:56:57 -0800

    Modify game.py

--- game.py
+++ game.py
@@ -1,17 +1,21 @@
 """
 Member List
-- Sally
-- Marti
-- Chansaa
-- Subin
+- Sally Poon
+- Martin Gatchev
+- Batchansaa Batzorig
+- Subin Moon
 
 Mar 13, 2021
 """
-
 import pygame
 import sys
 import random
 from pygame_functions import *
+
+
+# Game functions
+def draw_background():
+    pass
 
 
 pygame.init()
@@ -20,20 +24,25 @@ WINDOW_SIZE = (500, 500)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 clock = pygame.time.Clock()
 
-bg = pygame.image.load('/assets/background.jpg')
+# Game Variables
+bg_surface = pygame.image.load('/assets/background.jpg')
 
-running = True
+namu_surface = 1
 
-while running:
+game_active = True
 
+while True:
     for event in pygame.event.get():      # catch all the events that are happening right now
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    if game_active:
+        # Game functions ...
+        pass
 
     # image of player
     # background image
 
     pygame.display.update()
     clock.tick(120)
-
